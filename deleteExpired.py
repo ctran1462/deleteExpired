5f5a5b4ad9a8738a55f22aa6758159f5b04f12d7
Cam Tran <ctran1@u.brockport.edu>
2018-03-01 23:37:17 -0500

    added fuction to send email out if project is due within 5 days; needs Access/private key, auth SES email, and devops team email

--- deleteExpired.py
+++ deleteExpired.py
@@ -1,9 +1,59 @@
 import time
 import boto3
 import datetime
+import boto.ses
 now = datetime.datetime.now()											#cant just typecast to str because remainder of hours/mins/secs
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
+AWS_ACCESS_KEY = '' #insert aws access key/secret key of IAM user 
+AWS_SECRET_KEY = '' #
+#
+#
+#
+#
+class Email(object):  
+    def __init__(self, to, subject):
+        self.to = to
+        self.subject = subject
+        self._html = None
+        self._text = None
+        self._format = 'html'
+
+    def html(self, html):
+        self._html = html
+
+    def text(self, text):
+        self._text = text
+
+    def send(self, from_addr=None):
+        body = self._html
+
+        if isinstance(self.to, basestring):
+            self.to = [self.to]
+        if not from_addr:
+		#change to any email address verified through AWS SES
+            from_addr = 'ctran1@u.brockport.edu'
+        if not self._html and not self._text:
+            raise Exception('You must provide a text or html body.')
+        if not self._html:
+            self._format = 'text'
+            body = self._text
+
+        connection = boto.ses.connect_to_region(
+            'us-east-1',
+            aws_access_key_id=AWS_ACCESS_KEY, 
+            aws_secret_access_key=AWS_SECRET_KEY
+        )
+
+        return connection.send_email(
+            from_addr,
+            self.subject,
+            None,
+            self.to,
+            format=self._format,
+            text_body=self._text,
+            html_body=self._html
+        )
 #
 #
 #
@@ -63,6 +113,10 @@ for reservation in response["Reservations"]:
 	#432000 is the datetime float format's equivalent of 5 days
 	elif (inExpDate <= (now + 432000)) :
 		print("\n")
+		#change ctran1@u.brockport.edu to email of devops team/whom ever should be informed of a near expired instance
+		email = Email(to='ctran1@u.brockport.edu', subject='instance {}'.format(inID))
+		email.text('This is a warning for instance {}. project is due in 5 days or less'.format(inID))
+		email.send()
 		print("this needs an emailin'")
 		print("\n")
 	else :
