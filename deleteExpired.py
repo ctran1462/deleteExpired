87066fd1eae3d07438cc95a8d5298792f813cf75
Cam Tran <ctran1@u.brockport.edu>
2018-03-01 22:56:22 -0500

    refactored, added comments explaining process of code

--- deleteExpired.py
+++ deleteExpired.py
@@ -1,22 +1,27 @@
 import time
 import boto3
 import datetime
-now = datetime.datetime.now()
-#cant just typecast to str because remainder of hours/mins/secs    
+now = datetime.datetime.now()											#cant just typecast to str because remainder of hours/mins/secs
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
 #
+#
+#
+#takes in an instanceId(fid) and returns an instancename(instance exp date)
 def get_instance_date(fid):
     # When given an instance ID as str e.g. 'i-1234567', return the instance 'Name' from the name tag.
     ec2 = boto3.resource('ec2')
     ec2instance = ec2.Instance(fid)
-    instancename = ''
+    inExp = ''
     for tags in ec2instance.tags:
         if tags["Key"] == 'projectEndDate':
-            instancename = tags["Value"]
-    return instancename
-def delete_server(instanceId):
-                        conn.terminate_instances(instance_ids=[instanceId])
+            inExp = tags["Value"]
+    return inExp
+#
+#
+#
+#
+#nested for loop to iterate instances located in reservationsi
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
         now = datetime.datetime.now()
@@ -34,23 +39,33 @@ for reservation in response["Reservations"]:
 	print("\n")
 	inID = instance["InstanceId"]
 	inExpDate = get_instance_date(inID)
-	delete = ""
+	#standardizes date layout using excel datetime format(float)
 	inExpDate = time.mktime(time.strptime(inExpDate, "%Y-%m-%d"))
+	#make global 'now' into an str to be used in next line
 	now = now.strftime('%Y-%m-%d')
+	#excel datetime format(float)
 	now = time.mktime(time.strptime(now, "%Y-%m-%d"))
-	print(type(now))
-	print(type(inExpDate))
-	print(now)
-	print(inExpDate)
-	print(type(inID))
-	#to debug, getting error that one is a float
+#
+#	print(type(now))
+#       print(type(inExpDate))
+#       print(now)
+#       print(inExpDate)
+#       print(type(inID))
+        #to debug, getting error that one is a float
+	#
+	print("\n")
+	#
 	if(inExpDate <= now):
-		response = ec2client.terminate_instances(InstanceIds=[inID,],)
-		print("it is now, instance scheduled for deletion")
-	#else if (inExpDate == now + 5 days || inExpDate == now + 4 days || inExpDate == now + 3 days || inExpDate == now + 2 days || inExpDate == now + 1)
+		response = ec2client.terminate_instances(InstanceIds=[inID])
+		print("\n")
+		print("Instance scheduled for deletion")
+		print("\n")
 	#432000 is the datetime float format's equivalent of 5 days
-	if (inExpDate <= (now + 432000)) :
+	elif (inExpDate <= (now + 432000)) :
+		print("\n")
 		print("this needs an emailin'")
+		print("\n")
 	else :
-		print("Not now")
-	print (now + 345600)
+		print("\n")
+		print("project not due soon")
+		print("\n")
