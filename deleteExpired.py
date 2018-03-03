60e09b5f6c60a18a6eda0354fecc603cb9d35423
Cam Tran <ctran1@u.brockport.edu>
2018-03-03 13:47:30 -0500

    changed email function to utilize boto3, shouldnt require aws access/secret keys in lamda env

--- deleteExpired.py
+++ deleteExpired.py
@@ -2,61 +2,17 @@ import time
 import boto3
 import datetime
 import boto.ses
-now = datetime.datetime.now()											#cant just typecast to str because remainder of hours/mins/secs
+from botocore.exceptions import ClientError
+now = datetime.datetime.now()
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
-AWS_ACCESS_KEY = '' #insert aws access key/secret key of IAM user 
-AWS_SECRET_KEY = '' #
-#
-#
-#
-#
-class Email(object):  
-    def __init__(self, to, subject):
-        self.to = to
-        self.subject = subject
-        self._html = None
-        self._text = None
-        self._format = 'html'
+#change to authorized IAM user email
+SENDER = "Sender Name <ctran1@u.brockport.edu>"
+#change to desired recipients for expiration warning, needs to be SES verified
+RECIPIENT = "ctran1@u.brockport.edu"
+AWS_REGION = "us-east-1"
+client = boto3.client('ses',region_name=AWS_REGION)
 
-    def html(self, html):
-        self._html = html
-
-    def text(self, text):
-        self._text = text
-
-    def send(self, from_addr=None):
-        body = self._html
-
-        if isinstance(self.to, basestring):
-            self.to = [self.to]
-        if not from_addr:
-		#change to any email address verified through AWS SES
-            from_addr = 'ctran1@u.brockport.edu'
-        if not self._html and not self._text:
-            raise Exception('You must provide a text or html body.')
-        if not self._html:
-            self._format = 'text'
-            body = self._text
-
-        connection = boto.ses.connect_to_region(
-            'us-east-1',
-            aws_access_key_id=AWS_ACCESS_KEY, 
-            aws_secret_access_key=AWS_SECRET_KEY
-        )
-
-        return connection.send_email(
-            from_addr,
-            self.subject,
-            None,
-            self.to,
-            format=self._format,
-            text_body=self._text,
-            html_body=self._html
-        )
-#
-#
-#
 #takes in an instanceId(fid) and returns an instancename(instance exp date)
 def get_instance_date(fid):
     # When given an instance ID as str e.g. 'i-1234567', return the instance 'Name' from the name tag.
@@ -67,10 +23,8 @@ def get_instance_date(fid):
         if tags["Key"] == 'projectEndDate':
             inExp = tags["Value"]
     return inExp
-#
-#
-#
-#
+
+
 #nested for loop to iterate instances located in reservationsi
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
@@ -83,7 +37,6 @@ for reservation in response["Reservations"]:
 	print("\n")
 	print(ForKeyName)
 	#Just gives you the keys for the one instance atm, key being what is inside of the instance and value being the value of what was inside of the instance (2/20)
-	#                                                                      (what the instance consists of)
 	#we want the key "Tag"'s key and value
 	#we will need instance id to identify the instance to the machine
 	print("\n")
@@ -95,16 +48,9 @@ for reservation in response["Reservations"]:
 	now = now.strftime('%Y-%m-%d')
 	#excel datetime format(float)
 	now = time.mktime(time.strptime(now, "%Y-%m-%d"))
-#
-#	print(type(now))
-#       print(type(inExpDate))
-#       print(now)
-#       print(inExpDate)
-#       print(type(inID))
-        #to debug, getting error that one is a float
-	#
+
 	print("\n")
-	#
+
 	if(inExpDate <= now):
 		response = ec2client.terminate_instances(InstanceIds=[inID])
 		print("\n")
@@ -114,12 +60,53 @@ for reservation in response["Reservations"]:
 	elif (inExpDate <= (now + 432000)) :
 		print("\n")
 		#change ctran1@u.brockport.edu to email of devops team/whom ever should be informed of a near expired instance
-		email = Email(to='ctran1@u.brockport.edu', subject='instance {}'.format(inID))
-		email.text('This is a warning for instance {}. project is due in 5 days or less'.format(inID))
-		email.send()
-		print("this needs an emailin'")
-		print("\n")
-	else :
-		print("\n")
-		print("project not due soon")
-		print("\n")
+		SUBJECT = "Instance {}.".format(inID)
+		BODY_TEXT = ("This is a warning that instance {} is set to expire in 5 days or less.".format(inID))
+		CHARSET = "UTF-8"
+		BODY_HTML = """<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (SDK for Python)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-python/'>
+      AWS SDK for Python (Boto)</a>.</p>
+</body>
+</html>
+            """ 
+		try:
+    			#Provide the contents of the email.
+    			response = client.send_email(
+			        Destination={
+			            'ToAddresses': [
+			                RECIPIENT,
+			            ],
+			        },
+			        Message={
+			            'Body': {
+			                'Text': {
+			                    'Charset': CHARSET,
+			                    'Data': BODY_TEXT,
+			                },
+			            },
+			            'Subject': {
+			                'Charset': CHARSET,
+			                'Data': SUBJECT,
+			            },
+			        },
+			        Source=SENDER,
+			    )
+			# Display an error if something goes wrong.	
+		except ClientError as e:
+			print(e.response['Error']['Message'])
+		else:
+		    print("Email sent! Message ID:"),
+		    print(response['ResponseMetadata']['RequestId'])
+
+	print("this requires an email warning, email sent.")
+	print("\n")
+
+	#else :
+	#	print("\n")
+	#	print("project not due soon")
+	#	print("\n")
